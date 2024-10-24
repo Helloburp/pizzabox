623df75ea0e5c04a70eb8c26a879e5f3b7ea9d6d
Helloburp <thebosscda@gmail.com>
2024-10-24 01:22:26 -0500

    Added rig submenu with old rigging util tools.

--- src/init.server.lua
+++ src/init.server.lua
@@ -1,58 +1,51 @@
 assert(plugin, "Script must run as a plugin.")
 
 local Iris = require(script.Parent.Packages.Iris)
+local Ui = require(script.Ui)
 
-local MAIN_BUTTON_ID = "PIZZABOX"
-local VERSION = "0.1.0"
-local ICONS = {
-    Opened = "rbxassetid://119063127894512",
-    Closed = "rbxassetid://74405582882876"
+local C = require(script["Ui.constants"])
+local T = require(script["Ui.types"])
+
+local state: T.State = {
+    PluginOpenState = Iris.State(false),
+    SubMenuStates = {
+        Rig = Iris.State(false)
+    },
+    LastRigActionError = ""
 }
 
 local screenGui = Instance.new("ScreenGui", game:GetService("CoreGui"))
-local frame = Instance.new("Frame", screenGui)
-frame.Size = UDim2.fromScale(0.5,0.5)
-
-local toolbar = plugin:CreateToolbar(`PizzaBox v{VERSION}`)
-
-
+local toolbar = plugin:CreateToolbar(`PizzaBox v{C.VERSION}`)
 local button = toolbar:CreateButton(
-    MAIN_BUTTON_ID, "Toggles the PizzaBox menu.",
-    ICONS.Closed, "PizzaBox"
+    C.MAIN_BUTTON_ID, "Toggles the PizzaBox menu.",
+    C.ICONS.Closed, "PizzaBox"
 )
 
 
 function _setActive(state: boolean)
     button:SetActive(state)
     button.Icon = if state
-        then ICONS.Opened
-        else ICONS.Closed
+        then C.ICONS.Opened
+        else C.ICONS.Closed
     screenGui.Parent = if state
         then game:GetService("CoreGui")
         else script
 end
 
 
-local buttonState = Iris.State(false)
 button.Click:Connect(function()
-    buttonState:set(not buttonState:get())
-    _setActive(buttonState:get())
+    state.PluginOpenState:set(not state.PluginOpenState:get())
+    _setActive(state.PluginOpenState:get())
 end)
 
 
 plugin.Unloading:Connect(function()
-    buttonState:set(false)
+    state.PluginOpenState:set(false)
     _setActive(false)
+    Iris.Shutdown()
 end)
 
 Iris.Init(screenGui)
+Iris.UpdateGlobalConfig(C.IRIS_CONFIGS.RED_CONFIG)
 
-Iris:Connect(function()
-    if not buttonState:get() then return end
-    if Iris.Window(
-        "PizzaBox", {isOpened = buttonState}
-    ).closed() then
-        _setActive(false)
-    end
-    Iris.End()
-end)
+Iris:Connect(Ui.FromState(state, function() _setActive(false) end))
